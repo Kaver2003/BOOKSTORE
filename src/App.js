62e53920642f66173253a4e980394c40814177ff
Kaver2003 <karagodinaveronika18@gmail.com>
2025-03-24 15:16:32 +0900

    create empty componennts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-
-
 function App() {
     return (
         <div className="App">
-            <header className="bg-blue-600 p-4 text-white">
-yaa
+            <header className="">
+               <p className="">aaa</p>
             </header>
         </div>
     );
